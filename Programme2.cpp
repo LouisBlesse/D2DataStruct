18f0413d2504f22aca329c1cea60b74fbde8a5e2
Charles PTACEK <charles.ptacek@efrei.net>
2022-12-14 17:46:59 -0500

    Hyper graph ok, algo a faire

--- Programme2.cpp
+++ Programme2.cpp
@@ -1,7 +1,60 @@
 #include "Programme2.h"
 #include <iostream>
+#include "hypergraph.h"
+#include <fstream>
+
 using namespace std;
 
-void Programme2::Play() {
-	cout << "hello prog 2";
+void Indegree() {
+
 }
+
+void Pagerank() {
+		
+}
+
+void Programme2::Play() {
+
+	auto i = 0;
+	int id;
+	int id1;
+	int id2;
+	string outdegree;
+	string url;
+	Graph g;
+
+	ifstream Read_text1("C:/Users/Charles/Documents/UQAC/Structure_de_donnee/Projet2/Fichiers/Exo2/eu-2005.nodes.txt");
+
+	while (Read_text1 >> id >> outdegree >> url && i < 10000) {
+		Vertex v1;
+		v1.setID(id);
+		v1.setStateName(url);
+		g.addVertex(v1);
+		i++;
+	}
+
+	Read_text1.close();
+
+	ifstream Read_text2("C:/Users/Charles/Documents/UQAC/Structure_de_donnee/Projet2/Fichiers/Exo2/eu-2005.edges.txt");
+
+	i = 0;
+	while (Read_text2 >> id1 >> id2 && i < 10000) {
+		g.addEdgeByID(id1, id2, 0);
+		i++;
+	}
+
+	Read_text2.close();
+
+    g.printGraph();
+
+    system("PAUSE");
+	int choix;
+
+	cout << "Quelle algorithme voulez vous lancez ?\n1-Indegree\n2-PageRank" << endl;
+	cin >> choix;
+	if (choix == 1) {
+		Indegree();
+	}
+	else if (choix == 2) {
+		Pagerank();
+	}
